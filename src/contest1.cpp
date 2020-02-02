30a7729397aac4075c9442373145587de2ba36c1
Daniel <danielcampoverdev@gmail.com>
2020-02-02 01:15:51 -0600

    from drive

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -3,22 +3,182 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
-
 #include <stdio.h>
 #include <cmath>
-
 #include <chrono>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+#include <inttypes.h> 
+#include <iostream>
+#include <tuple>
+#define N_BUMPER (3)
+#define RAD2DEG(rad) ((rad)*180. / M_PI)
+#define DEG2RAD(deg) ((deg)*M_PI / 180.)
+float final_ori = 0.0;
+float final_ori_max =  0.0 ; 
+float final_ori_min = 0.0 ;
+bool  revolution =false; 
+float angular = 0.0;
+float linear = 0.0;
+float posX = 0.0, posY = 0.0, yaw = 0.0;
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+uint8_t leftState = bumper[kobuki_msgs::BumperEvent::LEFT];
 
+float minLaserDist = std::numeric_limits<float>::infinity();
+int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
 
-void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
 {
-	//fill with your code
+    //Access using bumper [kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
+    bumper[msg->bumper] = msg->state;
 }
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
-	//fill with your code
+    //fill with your code
+
+    minLaserDist = std::numeric_limits<float>::infinity();
+    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    desiredNLasers = desiredAngle * M_PI / (180 * msg->angle_increment);
+    //ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
+    if (desiredAngle * M_PI / 180 < msg->angle_max && -desiredAngle * M_PI / 180 > msg->angle_min)
+    {
+        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
+        {
+            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }
+    else
+    {
+        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
+        {
+            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }
 }
+
+
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    posX = msg->pose.pose.position.x;
+    posY = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+    tf::getYaw(msg->pose.pose.orientation);
+  //  ROS_INFO("Position:(%f,%f) Orientation: %f rad or %f degrees. ", posX, posY, yaw, RAD2DEG(yaw));
+}
+
+ float  sum_angles (float a, float b )
+ {
+     float sum= 0. ; 
+  if (a + b >= M_PI) 
+    {
+       sum = -M_PI + (a + b - M_PI) ;
+       return sum  ;
+    }
+    else if (a+b <= -M_PI   )
+    {
+       sum = M_PI  + (a+b + M_PI );
+       return sum;
+    }
+    else
+    {
+    sum = a+b ; 
+       return sum;
+    }  
+ }
+
+
+ bool Turn ( float angle, float *linear, float *angular , bool *check , float *final_ori , float *final_ori_max , float *final_ori_min)
+{  
+    // positive angle means turning CCW, negative angle means turning 
+     if (!*check)
+     {
+     float angler = 0.0; 
+    angler = DEG2RAD (angle) ; 
+    *final_ori = sum_angles (*final_ori , angler );
+    *final_ori_max= sum_angles(*final_ori , M_PI/90.);
+    *final_ori_min= sum_angles(*final_ori , -M_PI/90.) ; 
+    ROS_INFO("Expected angle at %f  %f   %f", RAD2DEG (*final_ori), RAD2DEG (*final_ori_max), RAD2DEG (*final_ori_min));
+    // checking for angle 
+    if (angle < 0 ) 
+    {
+     *angular = -M_PI/12. ;
+    }
+    else if (angle >0)
+    {
+     *angular = M_PI/12. ;
+    }
+    else 
+    {
+      *angular= 0.0  ;
+    } 
+
+      *linear = 0.0 ; 
+      *check = true ;
+      return false;
+    
+    }
+
+     else if (*check)
+    {
+              
+              if (yaw <= *final_ori_max  && yaw >  *final_ori_min && *final_ori_max > *final_ori_min)
+              {
+              *check = false ;
+              *angular= 0.0 ; 
+              *linear= 0.0; 
+              ROS_INFO("Rotation done");  
+              return true   ;       
+              }
+             else if (  *final_ori_max < *final_ori_min && (yaw <= *final_ori_max  ||  yaw  > *final_ori_min ))
+              {
+                *check = false ;
+                *angular = 0.0 ; 
+                *linear = 0.0; 
+                ROS_INFO("Rotation done");
+                return true ; 
+              }
+              else
+              {
+              
+              ROS_INFO("Expected angle  rot %f", RAD2DEG (*final_ori));
+              *check= true; 
+              return false ;
+              }
+
+    }
+}
+
+bool revolve (float *linear, float *angular, bool *revolution , bool *done , double *time)
+{
+  if ( !*revolution )
+        {    ROS_INFO("initializing revolution");
+            *angular = M_PI/6. ;
+            *linear = 0.0;
+            if (*time >=8 )
+            {
+             *revolution=true ;
+            }
+        }
+        else if (*revolution && *time >= 8. )
+        {
+            if ( yaw < (M_PI/90) && yaw >  (-M_PI/90)) 
+            {       
+                ROS_INFO("Revolution DONE ");    
+                *done =true ;
+                *revolution=false;  
+                *angular=0.0;
+            }
+            else
+            { 
+              *revolution=true; 
+              ROS_INFO("still rotating ");   
+                   
+            }
+        }
+}
+
 
 int main(int argc, char **argv)
 {
@@ -27,8 +187,9 @@ int main(int argc, char **argv)
 
     ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
     ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
-
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+
+    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
 
     ros::Rate loop_rate(10);
 
@@ -38,21 +199,87 @@ int main(int argc, char **argv)
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
+    
 
     float angular = 0.0;
     float linear = 0.0;
+    revolution= false; 
+    bool done = false;
+    bool check_left_done = false ;
+    bool check_right_done= false; 
+    float final_ori = 0.0;
+    float final_ori_max =  0.0 ; 
+    float final_ori_min = 0.0 ;
+    bool check_left= false;
+    bool check_right= false;
+    std::string process_state ="a";
 
-    while(ros::ok() && secondsElapsed <= 480) {
+    while (ros::ok() && secondsElapsed <= 480)
+    {
         ros::spinOnce();
-        //fill with your code
+        
+        double time = double (secondsElapsed);
+        ROS_INFO("lin: %f  ang:%f  time(%f )Pos: (%f, %f) yaw: %f laser: %f",linear, angular ,time,posX, posY, RAD2DEG(yaw), minLaserDist);
+          
+        bool any_bumper_pressed = false;
+        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx)
+         {
+             any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
+         }
 
+        if (!done )
+
+        {
+          revolve( &linear, &angular, &revolution , &done , &time);
+        }
+        
+
+        else if ( done && time >= 5. )
+        {
+            ROS_INFO("Starting procedure");   
+
+          if ( minLaserDist >= 0.60 && !any_bumper_pressed && process_state == "a" )
+          {
+            angular = 0.0;
+            linear = 0.2;
+            ROS_INFO("going straight ");
+            check_left_done= false ;
+            check_right_done=  false ;
+            
+          }  
+          else if ((minLaserDist <= 0.60 || any_bumper_pressed ) && process_state == "a"  )
+          {
+              process_state = "check_left";
+          }
+          else if ( process_state == "check_left")
+          {
+             
+            if ( check_left_done = Turn(90. , &linear, &angular , &check_left, &final_ori , &final_ori_max , &final_ori_min)) 
+              process_state = "check_right";
+             
+          }
+
+          else if ( process_state == "check_right" )
+
+          {  
+            if (check_right_done = Turn(-180. , &linear, &angular , &check_right, &final_ori , &final_ori_max , &final_ori_min))
+                process_state = "decide";
+               
+          }
+          else if (process_state == "decide")
+           {
+             ROS_INFO("DONE");
+           }
+            
+        }
+        
+        // The last thing to do is to update the timer.
+        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count();
+        loop_rate.sleep();
+        
         vel.angular.z = angular;
         vel.linear.x = linear;
         vel_pub.publish(vel);
-
-        // The last thing to do is to update the timer.
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
-        loop_rate.sleep();
     }
 
     return 0;
